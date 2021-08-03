5d082fc3f588c0b0593c03d3266a4db0146ab99b
Isaacgonzalez03 <Isaac.albert.g@gmail.com>
2021-08-03 16:57:10 -0600

    build

--- bug.client/src/env.js
+++ bug.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const domain = 'nitr0.us.auth0.com'
-export const audience = 'kanban.com'
-export const clientId = 'KGdIsFTOzS1Mz6apj46bl3AGsjzRgXv1'
+export const audience = 'nitr0.com'
+export const clientId = 'L0YLmj0kD9JBZ2aobsgJSeYrLz0rukrx'
